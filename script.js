d5768fcb3169e7484de986851b72a2679e73edb9
Luka-dev-sk8 <lukantecao@gmail.com>
2025-10-04 16:11:19 -0300

    Version 1.1

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const btn = document.getElementById('btn')
-const barra = document.querySelector('#barra')
+const btn = document.querySelector('#btn');
+const clicks = document.querySelector('.click');
+const barra = document.getElementById('barra');
 
-btn.addEventListener('click', () => {
-    barra.classList.toggle('.click')
+btn.addEventListener('click', () => { 
+    barra.classList.toggle('click');
+});
+
+document.addEventListener('click', function(e) {
+  if (barra.classList.contains('click') && !barra.contains(e.target) && !btn.contains(e.target)){
+        barra.classList.remove('click');
+  }
 });
